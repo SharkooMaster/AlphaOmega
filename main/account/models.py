a9a60be6a6ed3a07ba724cb1a33cfde959ebaa68
Alfred <alfred@stensatter.se>
2024-10-17 20:05:33 +0200

    added some stuff to the model

--- main/account/models.py
+++ main/account/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.forms import User
 from django.db import models
 
 # Create your models here.
 
 class Account (models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     home_screen_tags = models.TextField()
